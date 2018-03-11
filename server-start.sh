d762f3a29f8f5ccf13edba24e8c352a5249a0ef9
Chris Berthold <cberthold@technodotnet.com>
2018-03-11 15:36:58 -0400

    setup server start to build our config files and start zookeeper

--- server-start.sh
+++ server-start.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 
 # create properties files based on passed in environment vars
+/opt/kafka/bin/create-config-files.sh
 
 # start zookeeper
+/opt/kafka/bin/zookeeper-server-start.sh -daemon /opt/kafka/config/zookeeper.properties
 
-# wait a moment
+# wait a moment while ZooKeeper starts up
+#sleep 10s
 
 # start kafka
